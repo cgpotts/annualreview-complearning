a8970fde2d6fc62e4c1219692654c0c1b2896b24
Chris <chris@Christophers-iMac.local>
2014-05-25 22:39:42 -0700

    More efficient random choices; a comment relating SGD and Latent SGD

--- learning.py
+++ learning.py
@@ -34,10 +34,10 @@ __maintainer__ = "Christopher Potts"
 __email__ = "See the authors' websites"
 
 import re
+import random
 from collections import defaultdict
 from operator import itemgetter
 from itertools import product
-from random import shuffle
 
 def score(x=None, y=None, phi=None, w=None):
     """Calculates the inner product w * phi(x,y)."""
@@ -47,18 +47,30 @@ def predict(x=None, w=None, phi=None, classes=None, output_transform=(lambda x :
     scores = [(score(x, y_prime, phi, w), y_prime) for y_prime in classes(x)]
     # Get the maximal score:
     max_score = sorted(scores)[-1][0]
-    # Get all the candidates with the max score and chose one randomly:
+    # Get all the candidates with the max score and choose one randomly:
     y_hats = [y_alt for s, y_alt in scores if s == max_score]
-    shuffle(y_hats)            
-    y_hat = y_hats[0]
-    return output_transform(y_hat)
+    return output_transform(random.choice(y_hats))
+
+######################################################################
+# Note: SGD and LatentSGD can be seen as differing only in how they
+# choose the hidden variable y: for SGD, it is the same as the output
+# seen in the training data, whereas LatentSGD chooses it as the
+# highest scoring hidden variable. Thus, SGD and LatentSGD could be
+# stated as abstractions of a single function, call it GenericSGD,
+# differing only in the function used to choose this value: an
+# identity function for SGD and the best prediction for LatentSGD (see
+# the first line of the loop through the training data). We have not
+# combined them here in order to keep the code readable, but combining
+# could help bring out this insight (and make for more maintainable
+# code).
+######################################################################
 
 def SGD(D=None, phi=None, classes=None, T=10, eta=0.1, output_transform=None):
     """Implements stochatic (sub)gradient descent, as in the paper. classes should be a
     function of the input x for structure prediction cases (where classes is GEN)."""
     w = defaultdict(float)
     for t in range(T):
-        shuffle(D)
+        random.shuffle(D)
         for x, y in D:
             # Get all (score, y') pairs:
             scores = [(score(x, y_alt, phi, w)+cost(y, y_alt), y_alt) for y_alt in classes(x)]
@@ -66,8 +78,7 @@ def SGD(D=None, phi=None, classes=None, T=10, eta=0.1, output_transform=None):
             max_score = sorted(scores)[-1][0]
             # Get all the candidates with the max score and chose one randomly:
             y_tildes = [y_alt for s, y_alt in scores if s == max_score]
-            shuffle(y_tildes)            
-            y_tilde = y_tildes[0]
+            y_tilde = random.choice(y_tildes)
             # Weight-update (a bit cumbersome because of the dict-based implementation):
             actual_rep = phi(x, y)
             predicted_rep = phi(x, y_tilde)
@@ -81,7 +92,7 @@ def LatentSGD(D=None, phi=None, classes=None, T=10, eta=0.1, output_transform=No
     each input x."""
     w = defaultdict(float)
     for t in range(T):
-        shuffle(D)
+        random.shuffle(D)
         for x, d in D:
             # Get the best viable candidate given the current weights:
             y = predict(x, w, phi=phi, classes=(lambda z : [zd for zd in classes(z) if output_transform(zd) == d]))
@@ -91,8 +102,7 @@ def LatentSGD(D=None, phi=None, classes=None, T=10, eta=0.1, output_transform=No
             max_score = sorted(scores)[-1][0]
             # Get all the candidates with the max score and chose one randomly:
             y_tildes = [y_alt for s, y_alt in scores if s == max_score]
-            shuffle(y_tildes)            
-            y_tilde = y_tildes[0]
+            y_tilde = random.choice(y_tildes)
             # Weight-update:
             actual_rep = phi(x, y)
             predicted_rep = phi(x, y_tilde)
