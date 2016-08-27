0be975ef601a97f3de47a45a89909ba6fe31a86a
Christopher Potts <cgpotts@gmail.com>
2016-08-27 11:22:16 -0700

    Added for package use

---
+++ __init__.py
@@ -0,0 +1,7 @@
+__author__ = "Christopher Potts and Percy Liang"
+__copyright__ = "Copyright 2014-, Christopher Potts and Percy Liang"
+__credits__ = []
+__license__ = "GNU general public license, version 2"
+__version__ = "2.0"
+__maintainer__ = "Christopher Potts"
+__email__ = "See the authors' websites"
